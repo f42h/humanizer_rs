d88ac02e1e4656eb1323a1a64ffda63d30324f47
f42h <felix.hansow@tuta.com>
2025-07-23 11:01:14 +0200

    Improve generator
    - Ensure to include both, special chars and year within a password at the same time
    - Seperated generation is still included

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 use std::{
     collections::HashMap, 
     fmt::Debug, 
@@ -57,33 +58,38 @@ fn if_contains_replace(keyword: &mut String, to_check: &str, replace_if: &str) {
     }
 }
 
-fn add_elements<T: std::fmt::Display>(
-    collection: &Vec<T>, 
-    password_pool: &mut Vec<String>, 
-    variation: &mut String
-) {
-    for element in collection {
-        password_pool.push(format!("{}{}", variation, element));
-
-        for n in 0..variation.len() {
-            password_pool.push(format!("{}{}{}", &variation[..n], element, &variation[n..]));
-        }
-    }
-}
-
 fn append_password(file: &mut File, password: &str) {
     file.write_all(format!("{}\n", password).as_bytes())
         .expect("Unable to append data");
     file.flush().unwrap();
 }
 
+fn add_elements(
+    years: &Vec<u16>, 
+    specials: &Vec<char>, 
+    password_pool: &mut Vec<String>, 
+    variation: &str
+) {
+    for year in years {
+        for ch in specials {
+            password_pool.push(format!("{}{}{}", variation, year, ch)); 
+            password_pool.push(format!("{}{}{}", year, ch, variation)); 
+            password_pool.push(format!("{}{}{}", variation, ch, year)); 
+            password_pool.push(format!("{}{}{}", year, variation, ch)); 
+        }
+    }
+}
+
 fn generate_passwords(keywords: Vec<String>, filename: &str, year_from: u16, year_to: u16) {
     let years = gen_years(year_from, year_to).unwrap();
     let specials = special_chars();
     let alt_chars = human_alternatives();
+
     let mut generated_passwords = Vec::new();
     let original = keywords[0].clone();
-    let mut count = 0;
+    let mut password_count = 0;
+    let iter_count = 2; // To ensure passwords are generated with and without human alternativ chars
+    let mut iter_control = false;
 
     if Path::new(filename).exists() {
         remove_file(filename).expect(&format!("Unable to remove: {}", filename));
@@ -95,56 +101,64 @@ fn generate_passwords(keywords: Vec<String>, filename: &str, year_from: u16, yea
         .open(filename)
         .expect("Unable to open file");
 
-    for mut keyword in keywords {
-        // Replace characters based on alternatives
-        for (alt, classic) in &alt_chars {
-            if_contains_replace(&mut keyword, "i", "1");
-            if_contains_replace(&mut keyword, "e", "3");
-            if_contains_replace(&mut keyword, &classic.to_string(), &alt.to_string());
-        }
-
-        // Generate case variations
-        let keyword_chars: Vec<char> = keyword.chars().collect();
-        let keyword_length = keyword_chars.len();
-        let variations_count = 1 << keyword_length; // 2^length for all combinations
-
-        for current in 0..variations_count {
-            let mut variation = String::new();
-
-            // Consider case variations
-            for (idx, &c) in keyword_chars.iter().enumerate() {
-                if (current & (1 << idx)) != 0 {
-                    variation.push(c.to_uppercase().next().unwrap());
-                } else {
-                    variation.push(c);
+    for _ in 0..iter_count {
+        generated_passwords.clear();
+        
+        for mut keyword in keywords.clone() {
+            if iter_control {
+                // Replace characters based on alternatives
+                for (alt, classic) in &alt_chars {
+                    if_contains_replace(&mut keyword, "i", "1");
+                    if_contains_replace(&mut keyword, "e", "3");
+                    if_contains_replace(&mut keyword, &classic.to_string(), &alt.to_string());
                 }
             }
 
-            // Include years and special chars to each element in password pool
-            add_elements(&years, &mut generated_passwords, &mut variation);
-            add_elements(&specials, &mut generated_passwords, &mut variation);
+            // Generate case variations
+            let keyword_chars: Vec<char> = keyword.chars().collect();
+            let keyword_length = keyword_chars.len();
+            let variations_count = 1 << keyword_length; // 2^length for all combinations
+
+            for current in 0..variations_count {
+                let mut variation = String::new();
+
+                // Consider case variations
+                for (idx, &c) in keyword_chars.iter().enumerate() {
+                    if (current & (1 << idx)) != 0 {
+                        variation.push(c.to_uppercase().next().unwrap());
+                    } else {
+                        variation.push(c);
+                    }
+                }
+
+                // Include years and special chars to each element in password pool
+                add_elements(&years, &specials, &mut generated_passwords, &mut variation);
+            }
+
+            // Append original keyword
+            append_password(&mut file, &original);
+
+            // Append recently generated passwords for each keyword
+            for password in &generated_passwords {
+                append_password(&mut file, password);
+                password_count += 1;
+
+                print!(
+                    "\r{} passwords generated,current: {}{}", 
+                    password_count, password, " ".repeat(20)
+                );
+                stdout().flush().unwrap();
+            }
         }
 
-        // Append original keyword
-        append_password(&mut file, &original);
-
-        // Append recently generated passwords for each keyword
-        for password in &generated_passwords {
-            append_password(&mut file, password);
-            count += 1;
-
-            print!(
-                "\r{} passwords generated,current: {}{}", 
-                count, password, " ".repeat(20)
-            );
-            stdout().flush().unwrap();
-        }
+        iter_control = !iter_control;
     }
 
     print!("\r");
-    println!("\rDone, generated {} passwords in total!{}", count, " ".repeat(10));
+    println!("\rDone, generated {} passwords in total!{}", password_count, " ".repeat(10));
     println!(" ==> Output saved to `{}`", filename)
 }
+
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
